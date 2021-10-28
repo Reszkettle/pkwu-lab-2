4f1a6604b28cd02235ab92dd7968d9471c91682c
Rechu <reszkettle@gmail.com>
2021-10-28 13:43:48 +0200

    Implemented analyse_string endpoint to return data after analysis

--- main.py
+++ main.py
@@ -6,6 +6,14 @@ import dependencies
 app = FastAPI()
 
 
-@app.get(path="/analyse-string", response_model=schemas.OutAnalyseString)
+@app.post(path="/analyse-string", response_model=schemas.OutAnalyseString)
 async def analyse_string(request: schemas.InAnalyseString, analyser: services.StringService = Depends(dependencies.string_analyser)):
-    ...
+    """ Analyses given string """
+    result = analyser.analyse(request.string, request.substring)
+    return schemas.OutAnalyseString(
+        digits_count=result.digits_count,
+        lower_case_letters_count=result.lower_case_letters_count,
+        upper_case_letters_count=result.upper_case_letters_count,
+        special_characters_count=result.special_characters_count,
+        substring_occurrences_count=result.substring_occurrences_count,
+    )
