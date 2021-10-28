47a9c7a52662c801a367e706902ddbea07c12fae
Rechu <reszkettle@gmail.com>
2021-10-28 13:09:29 +0200

    Created In and Out scheams for AnalyseString

---
+++ schemas.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel
+from typing import Optional
+
+
+class InAnalyseString(BaseModel):
+    string: str
+    substring: Optional[str]
+
+
+class OutAnalyseString(BaseModel):
+    lower_case_letters_count: int
+    upper_case_letters_count: int
+    digits_count: int
+    special_characters_count: int
+    substring_occurrences_count: Optional[int]
