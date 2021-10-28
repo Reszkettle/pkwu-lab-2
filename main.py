cf30af39ce7cd8a3f3e80ab0f0c08db9dc6936aa
Rechu <reszkettle@gmail.com>
2021-10-28 13:29:38 +0200

    Added string_analyser dependency into analyse_string endpoint

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 import schemas
+import services
+import dependencies
 
 app = FastAPI()
 
 
 @app.get(path="/analyse-string", response_model=schemas.OutAnalyseString)
-async def analyse_string(request: schemas.InAnalyseString):
+async def analyse_string(request: schemas.InAnalyseString, analyser: services.StringService = Depends(dependencies.string_analyser)):
     ...
